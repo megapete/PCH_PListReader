82cdcacf212f69d922ec7b7612a8d5f64ee4e673
Peter C Huber <peter@hubersitech.com>
2019-12-18 18:43:55 -0500

    Initial Commit

---
+++ PCH_PListReader/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PCH_PListReader
+//
+//  Created by Peter Huber on 2019-12-18.
+//  Copyright © 2019 Peter Huber. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
